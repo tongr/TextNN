bd38666a9a77224d4f2344294a4f9f589b04a368
tongr <tongr@online>
2019-02-20 23:14:31 +0100

    [#9] add util to create plot (Matplotlib)

--- textnn/utils/__init__.py
+++ textnn/utils/__init__.py
@@ -1,4 +1,6 @@
-from typing import Iterable
+import logging
+from pathlib import Path
+from typing import Dict, Iterable, List
 
 from keras.utils.generic_utils import Progbar, time
 
@@ -61,3 +63,44 @@ class ProgressIterator(Progbar):
             self._last_update = 0
             self._seen_so_far = self._progress
             self.update(self.target if self.target is not None else self._progress)
+
+
+def plot2file(file: Path, x_values: list, y_series: Dict[str, list],
+              title: str = None, x_label: str = None, y_label: str = None, series_styles: List[str] = None):
+    """
+    plot the given data to a file
+    :param file: storage location of the plot
+    :param x_values: list of x values to plot
+    :param y_series: dictionary of named(key) lists containing the y values to plot
+    :param title: the plot title
+    :param x_label: the x axis label
+    :param y_label: the y axis label
+    :param series_styles: the styles of the `y_series` (see "Format Strings" in
+    https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html)
+    """
+    try:
+        import matplotlib.pyplot as plt
+        # inspired by the tensorflow introduction of François Chollet
+        # https://www.tensorflow.org/tutorials/keras/basic_text_classification
+        if not series_styles:
+            # default styles: iterate through different combinations
+            series_styles = list(f"{color}{marker}{line}"
+                                for line in "- -- -. :".split()
+                                for marker in " o v ^ s P X".split(" ")
+                                for color in "b r g c m y k".split())
+
+        for idx, (series_label, y_values) in enumerate(y_series.items()):
+            style = series_styles[idx % len(series_styles)]
+            plt.plot(x_values, y_values, style, label=series_label)
+
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.legend()
+
+        if not file.parent.exists():
+            file.parent.mkdir(exist_ok=True, parents=True)
+        plt.savefig(str(file))
+        plt.clf()
+    except ImportError:
+        logging.warning(f"Unable to create plot in {file}, missing Matplotlib dependency!")
