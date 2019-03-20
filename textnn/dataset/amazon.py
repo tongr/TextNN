46225158d422dc0797c51515795ed06b34f290e1
tongr <tongr@online>
2019-03-20 17:02:17 +0100

    [#14] add amazon dataset file utils

---
+++ textnn/dataset/amazon.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Iterable, Tuple
+
+from textnn.utils import read_text_file_lines as read_lines
+
+
+def amazon_star_rating_generator(dataset_file: Path) -> Iterable[Tuple[str, int]]:
+    """
+    Generate a text to star-rating tuples from a review tsv file.
+    :param dataset_file: the tsv file containing the reviews
+    :return: an iterable over the reviews from `dataset_file`
+    """
+    def get_text_and_label(line: str) -> Tuple[str, int]:
+        fields = line.split("\t")
+        return " ".join(fields[12:14]), int(fields[7])
+
+    return (get_text_and_label(line) for line in read_lines(file_path=dataset_file, ignore_first_n_lines=1))
+
+
+def amazon_binary_review_generator(dataset_file: Path, label_3_stars_as=None,
+                                   ) -> Iterable[Tuple[str, int]]:
+    """
+    Generate a text to binary-label tuples from a review tsv file.
+    :param dataset_file: the tsv file containing the reviews
+    :param label_3_stars_as: specify the binary label for 3-star reviews
+    :return: an iterable over the reviews from `dataset_file`
+    """
+    def stars_to_binary(rating):
+        if rating == 3:
+            return label_3_stars_as
+        return 0 if rating < 3 else 1
+
+    # transform each rating according to stars_to_binary
+    binary_ratings = ((text, stars_to_binary(stars)) for text, stars in amazon_star_rating_generator(
+        dataset_file=dataset_file))
+
+    # remove None-labels
+    return filter(lambda tup: tup[1] is not None, binary_ratings)
